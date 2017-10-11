426bb815a2975bd33caa1b3a6fbd5f04622d1ce6
201586-\Nakama <dhanidwiputra123@gmail.com>
2017-10-11 07:42:12 +0700

    view trailer

---
+++ application/views/trailer.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<?php include('static/helmet.php'); ?>
+
+<?php include('static/header.php'); ?>
+
+<title>Save The Pure | Trailer</title>
+<div class="wrapper-container u-py3" style="min-height: 600px;">
+
+    <div class="img-card" href="">
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/vgBAkUsnsw4?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+        </div>
+    </div>
+
+    <h2 class="center">Trailer</h2>
+	
+	<div class="col-6 mx-auto mt2 py2 px2">
+		<p class="justify">
+	        We present you a brief tease of the hidden struggles beneath the city. We have approached worthy
+            candidates in the city with the intention to lend a hand, and the short clip will give an overview of the process. 
+            The forgotten dreams have been burried for way too long, dreams that can only flourish again when action takes part. And we intend to act. 
+	    </p>
+	</div>
+</div>
+
+<?php include('static/footer.php'); ?>
