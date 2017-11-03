3b9db5bec912c3df1f9dd9b9e108ca8928dad1e4
ZENBOOK\seza <seznant4@gmail.com>
2017-11-03 11:45:47 +0700

    push halaman testing branch

---
+++ application/views/halaman_seza.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ini halamanku</title>
+</head>
+<body>
+<h1>Testing Halaman Seza</h1>
+<a href="<?php echo base_url() ?>halaman_seza/pertama">Testing</a>
+<?php echo $nama; ?>
+</body>
+</html>
