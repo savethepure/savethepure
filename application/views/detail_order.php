6b54d0848784b7662846db970f571baa474f9457
ZENBOOK\seza <seznant4@gmail.com>
2017-11-04 13:45:00 +0700

    UI admin terima barang

---
+++ application/views/detail_order.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php include('static/helmet.php'); ?>
+	 <link href="<?php echo base_url() ?>assets/see_order_assets/css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="row">
+		<!-- Basic Table -->
+		<div class="col-sm-12">
+			<div class="panel panel-default card-view">
+				<div class="panel-heading">
+					<div class="pull-left">
+						<h6 class="panel-title txt-dark">Basic Table</h6>
+						<table>
+							<tr>
+								<td><h6>Order ID</h6></td>
+								<td>&nbsp;</td>
+								<td><h6>2734872834</h6></td>
+							</tr>
+							<tr>
+								<td><h6>User ID</h6></td>
+								<td>&nbsp;</td>
+								<td><h6>2734872834</h6></td>
+							</tr>
+							<tr>
+								<td><h6>User Name</h6></td>
+								<td>&nbsp;</td>
+								<td><h6>2734872834</h6></td>
+							</tr>
+							<tr>
+								<td><h6>Receiver Name </h6></td>
+								<td>&nbsp;</td>
+								<td><h6>2734872834</h6></td>
+							</tr>
+							<tr>
+								<td><h6>Shipment Address</h6></td>
+								<td>&nbsp;</td>
+								<td><h6>2734872834</h6></td>
+							</tr>
+							
+						</table>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="panel-wrapper collapse in">
+					
+								<table class="table mb-0">
+									<thead>
+									  <tr>
+										<th>#</th>
+										<th>Product Name</th>
+										<th>Color</th>
+										<th>Size</th>
+										<th>Qty</th>
+										<th>Total Price</th>
+									
+									  </tr>
+									</thead>
+									<tbody>
+									  <tr>
+										<td>1</td>
+										<td>Product Name</td>
+										<td>Jens</td>
+										<td>Brincker</td>
+										<td>Brincker123</td>
+										<td>310000</td>
+										<td class="form-group mb-0">
+											<button type="submit" class="btn btn-info ">Detail</button>
+										</td>			
+									  </tr>
+								
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /Basic Table -->
+	</div>	
+
+</body>
+</html>
---
+++ application/views/see_order.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<?php include('static/helmet.php'); ?>
+	 <link href="<?php echo base_url() ?>assets/see_order_assets/css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="row">
+		<!-- Basic Table -->
+		<div class="col-sm-12">
+			<div class="panel panel-default card-view">
+				<div class="panel-heading">
+					<div class="pull-left">
+						<h6 class="panel-title txt-dark">Basic Table</h6>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+				<div class="panel-wrapper collapse in">
+					
+								<table class="table mb-0">
+									<thead>
+									  <tr>
+										<th>#</th>
+										<th>Order ID</th>
+										<th>Name</th>
+										<th>Qty</th>
+									
+									  </tr>
+									</thead>
+									<tbody>
+									  <tr>
+										<td>1</td>
+										<td>Jens</td>
+										<td>Brincker</td>
+										<td>Brincker123</td>
+										<td class="form-group mb-0">
+											<button type="submit" class="btn btn-info ">Detail</button>
+										</td>			
+									  </tr>
+								
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /Basic Table -->
+	</div>	
+
+</body>
+</html>
